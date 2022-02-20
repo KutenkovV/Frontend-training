253f3bcba60d4eb3973b8cbde03569967483a886
Владислав Кутенков <kutenkovvlad017@gmail.com>
2022-02-21 00:24:06 +0800

    Drag & Drop ака Трелло начал делать аналог трелло в плане марафона сделал менюшку, решил не пользоваться бутстрапом

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
-// в переменную slides собираем все объекты с классом "slide" т.е. все картинки
-const slides = document.querySelectorAll('.slide')
+function slidesPlagin(activeSlide = 0) {
+    // в переменную slides собираем все объекты с классом "slide" т.е. все картинки
+    const slides = document.querySelectorAll('.slide')
 
-//  перебираем каждый слайд и добавляем на него слушателя
-for(const slide of slides) {
-    slide.addEventListener('click', ()=> {
-        clearActiveClasses()
+    slides[activeSlide].classList.add('active')
 
-        slide.classList.add('active')
-    })
+    //  перебираем каждый слайд и добавляем на него слушателя
+    for(const slide of slides) {
+        slide.addEventListener('click', ()=> {
+            clearActiveClasses()
+
+            slide.classList.add('active')
+        })
+    }
+
+    // удаляем активный класс у активного слайдера
+    function clearActiveClasses() {
+        slides.forEach((slide) => {
+            slide.classList.remove('active')
+        })
+    }
 }
 
-// удаляем активный класс у активного слайдера
-function clearActiveClasses() {
-    slides.forEach((slide) => {
-        slide.classList.remove('active')
-    })
-}
+slidesPlagin(0)
